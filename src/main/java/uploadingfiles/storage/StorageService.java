020f4509bb4237fd985ae1be43c6d7bdef0ed2b0
caoxiang <caoxiang@xiaomi.com>
2021-10-21 16:47:16 +0800

    upload-file-app

---
+++ src/main/java/uploadingfiles/storage/StorageService.java
@@ -0,0 +1,21 @@
+package uploadingfiles.storage;
+
+import java.util.stream.Stream;
+import java.nio.file.Path;
+
+import org.springframework.core.io.Resource;
+import org.springframework.web.multipart.MultipartFile;
+
+public interface StorageService {
+    void init();
+
+    void store(MultipartFile file);
+
+    Stream<Path> loadAll();
+
+    Path load(String filename);
+
+    Resource loadAsResource(String filename);
+
+    void deleteAll();
+}
